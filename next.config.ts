9aedbdc90622d1d2f8efd459d92c9f926784d606
Toby <tobypang@me.com>
2026-03-19 17:27:14 +0800

    feat: enable toby.courses → ateliertoby.com 301 redirect

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // TODO: Enable redirects after ateliertoby.com SSL is provisioned
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: "/:path*",
-  //       has: [{ type: "host", value: "toby.courses" }],
-  //       destination: "https://ateliertoby.com/:path*",
-  //       permanent: true,
-  //     },
-  //     {
-  //       source: "/:path*",
-  //       has: [{ type: "host", value: "www.toby.courses" }],
-  //       destination: "https://ateliertoby.com/:path*",
-  //       permanent: true,
-  //     },
-  //   ];
-  // },
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "toby.courses" }],
+        destination: "https://ateliertoby.com/:path*",
+        permanent: true,
+      },
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "www.toby.courses" }],
+        destination: "https://ateliertoby.com/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
